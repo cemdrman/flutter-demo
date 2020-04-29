c37731295fb46eb91325a2a95801d40a9d4642f5
Cem Dırman <cemdrman@gmail.com>
2020-04-30 00:08:52 +0300

    safeArea, clildren with widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Colors.white,
-    appBar: AppBar(
-      title: Text("App Bar"),
-      backgroundColor: Colors.indigoAccent,
-    ),
-    body: Center(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Image(
-         image: AssetImage('images/planet.jpg')
-        ),
-      ),
-    ),
-  )));
+  runApp(MypApp() );
 }
+
+
+class MypApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Text('white'),
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  padding: EdgeInsets.all(20.0),
+                ),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Colors.indigoAccent,
+                  child: Text('indigoAccent'),
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  padding: EdgeInsets.all(20.0),
+                ),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Colors.yellow,
+                  child: Text('yellow'),
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  padding: EdgeInsets.all(20.0),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+
+
