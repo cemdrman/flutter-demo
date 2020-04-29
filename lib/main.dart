a9a01fc868c7345be4c44256b0070031624ff8bc
Cem Dırman <cemdrman@gmail.com>
2020-04-30 00:55:22 +0300

    circleAvatar, icon, row type container

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,33 +15,80 @@ class MypApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
             child: Column(
-
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
+                CircleAvatar(
+                  radius: 75.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('images/travel.jpg'),
+
+                ),
+                Text(
+                  'Cem DIRMAN',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Lobster',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  'Java Developer',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Lobster',
+                    color: Colors.white
+                  ),
+                ),
                 Container(
-                  height: 100,
-                  width: double.infinity,
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                   color: Colors.white,
-                  child: Text('white'),
-                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        size: 20,
+                        color: Colors.teal
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '555-444-33-22',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20,
+
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 Container(
-                  height: 100,
-                  width: double.infinity,
-                  color: Colors.indigoAccent,
-                  child: Text('indigoAccent'),
-                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  padding: EdgeInsets.all(20.0),
-                ),
-                Container(
-                  height: 100,
-                  width: double.infinity,
-                  color: Colors.yellow,
-                  child: Text('yellow'),
-                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  padding: EdgeInsets.all(20.0),
-                ),
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                          Icons.email,
+                          size: 20,
+                          color: Colors.teal
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'cem.dirman@bilisim.io',
+                         style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20,
+
+                        ),
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
